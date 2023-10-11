4ab99343cb2e6688b7733d5019faa6959e3442de
Wilson <wlei6251@bths.edu>
2023-10-11 19:53:04 -0400

    added enter button

--- scripting.js
+++ scripting.js
@@ -1,2 +1,30 @@
 let clock = 7;
+let random;
+let answer;
+
+    function init() {
+        document.getElementById("clockNum").innerHTML = clock;
+        random = Math.floor(Math.random() * 3) + 1;
+    }
+
+    function guess() {
+        clock--;
+        document.getElementById("clockNum").innerHTML = clock;
+            
+    }
+
+    function storeAnswer() {
+        document.querySelector('#buttons')
+        .addEventListener('click', event => {
+        let target = event.target;
+        if (target.matches('button')) {
+      let value = target.innerHTML
+      
+      document.querySelector('#output').value += value
+    }
+    });
+    }
+
+
+
 
